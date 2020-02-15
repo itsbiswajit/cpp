81729d43f55ea2e187d8cbb97389c121a335d7ba
Biswajit Halder <kgecit.biswajit@gmail.com>
2020-02-15 18:53:32 +0530

    intro

---
+++ HelloWorld/helloworld.cpp
@@ -0,0 +1,10 @@
+// First program in C++
+// This Code will print Hello World!
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+   cout << "Hello World!"; // prints Hello World
+   return 0;
+}
